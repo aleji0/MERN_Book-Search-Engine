69f9d17b050d70a2e81a2b7ac209c894e4559e05
aleji0 <alex.springer@gmail.com>
2022-12-01 12:50:07 -0700

    type defs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    password: String
+    email: String
+  }  
+  
+  type Pet {
+    _id: ID
+    petName: String
+    petType: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: User
+    users: [User]!
+    getParks: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+
+    addPet(
+      petName: String!
+      petType: String!
+    ) : Pet
+    updatePet(
+      petName: String
+      petType: String
+    ) : Pet
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
+  }
+`;
+
+module.exports = typeDefs;
